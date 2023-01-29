6fea000e426380f30325ada23e3e45363979b1bd
Jérémy Sam-tow-weng <58853119+kuroho@users.noreply.github.com>
2023-01-30 00:21:05 +0100

    Buy champion
    Buy champion

---
+++ Assets/Scripts/ButtonHelper/BuyButtonMgr.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuyButtonMgr : MonoBehaviour
+{
+    public Button button;
+
+    public void OnButtonPress() {
+        Debug.Log("clicked");
+    }
+}
